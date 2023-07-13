ac4a014a17b48c554532d67daeeb2304a87514a1
Gulce Batgi <elifgulcebatgi33@gmail.com>
2023-07-13 22:40:02 +0300

    ayran

--- script.js
+++ script.js
@@ -2,20 +2,15 @@ const usernameInput = document.getElementById('username');
 const passwordInput = document.getElementById('password');
 const submitButon = document.getElementById('buton');
 
-submitButon.addEventListener('click', (e) => {
-    e.preventDefault();
-    
-
+submitButon.addEventListener('click', function () {
     const username = usernameInput.value; 
     const password = passwordInput.value; 
-
-
-
-
     authenticateUser (username, password);
 });
-
 function authenticateUser (username, password){
-
+    console.log ("auuuuuuu");
+    if(username == "HevselSSL" && password == "q9w12E14M218120502") {
+        console.log ("if");
+        window.location.replace ("ssl.html");
+    }
 }
-
