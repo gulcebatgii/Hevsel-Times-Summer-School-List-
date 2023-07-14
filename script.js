7bc63d41eb2fd565e657d91d9c9589aad6de829f
Gulce Batgi <elifgulcebatgi33@gmail.com>
2023-07-14 23:58:21 +0300

    hahahabitti

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ const usernameInput = document.getElementById('username');
 const passwordInput = document.getElementById('password');
 const submitButon = document.getElementById('buton');
 
-submitButon.addEventListener('click', function () {
+submitButon.addEventListener('click', (event) => {
     const username = usernameInput.value; 
     const password = passwordInput.value; 
     authenticateUser (username, password);
-});
+})
 function authenticateUser (username, password){
     console.log ("auuuuuuu");
     if(username == "HevselSSL" && password == "q9w12E14M218120502") {
         console.log ("if");
-        window.location.replace ("ssl.html");
+        window.location.replace ("list/ssl.html");
     }
 }
