81a3b6a279650de46b4bd758321e42024088b59e
Poyraz Bahcivan <poyrazbahcivan@gmail.com>
2023-07-14 20:29:21 +0300

    cozduuum :))

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ const usernameInput = document.getElementById('username');
 const passwordInput = document.getElementById('password');
 const submitButon = document.getElementById('buton');
 
-submitButon.addEventListener('click', function () {
-    const username = usernameInput.value; 
+submitButon.addEventListener('click', (event) => {
+	const username = usernameInput.value; 
     const password = passwordInput.value; 
     authenticateUser (username, password);
-});
+})
+
 function authenticateUser (username, password){
     console.log ("auuuuuuu");
     if(username == "HevselSSL" && password == "q9w12E14M218120502") {
