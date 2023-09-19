d33c1fed8d454f7fecd7fe8aafdb38b719149a32
Poyraz Bahcivan <poyrazbahcivan@gmail.com>
2023-09-20 00:45:55 +0300

    fix

---
+++ check.js
@@ -0,0 +1,12 @@
+    // Check if the user is authenticated; if not, redirect to the login page
+    if (!authenticatedUser()) {
+        window.location.href = '../index.html';
+    }
+
+    // Your main page code goes here
+
+    function authenticatedUser() {
+        // Check if the 'authenticated' flag is set to 'true' in localStorage
+        console.log(localStorage.getItem('authenticated'));
+        return localStorage.getItem('authenticated') === 'true';
+    }
