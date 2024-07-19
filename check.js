19bff26ceaaad02876f069eded11f379ca7f11ec
YigitEfeNas <106618241+YigitEfeNas@users.noreply.github.com>
2024-07-19 23:24:04 +0300

    Update check.js

--- check.js
+++ check.js
@@ -1,12 +1,13 @@
-    // Check if the user is authenticated; if not, redirect to the login page
     if (!authenticatedUser()) {
         window.location.href = '../index.html';
     }
 
-    // Your main page code goes here
 
     function authenticatedUser() {
         // Check if the 'authenticated' flag is set to 'true' in localStorage
         console.log(localStorage.getItem('authenticated'));
         return localStorage.getItem('authenticated') === 'true';
     }
+    window.addEventListener("beforeunload", function(e){
+        localStorage.setItem('authenticated', 'false');
+     });
