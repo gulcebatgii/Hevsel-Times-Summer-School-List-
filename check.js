f94698c1c2c49d3b0b0bd587552773c5a505c92f
YigitEfeNas <106618241+YigitEfeNas@users.noreply.github.com>
2024-07-20 10:48:47 +0300

    Update check.js

--- check.js
+++ check.js
@@ -1,13 +1,32 @@
-    if (!authenticatedUser()) {
-        window.location.href = '../index.html';
-    }
+if (!authenticatedUser()) {
+    window.location.href = '../index.html';
+}
 
+function authenticatedUser() {
+    console.log(localStorage.getItem('authenticated'));
+    return localStorage.getItem('authenticated') === 'true';
+}
 
-    function authenticatedUser() {
-        // Check if the 'authenticated' flag is set to 'true' in localStorage
-        console.log(localStorage.getItem('authenticated'));
-        return localStorage.getItem('authenticated') === 'true';
-    }
-    window.addEventListener("beforeunload", function(e){
+function updateTabCount(increment) {
+    let tabCount = parseInt(sessionStorage.getItem('tabCount')) || 0;
+    tabCount += increment;
+    sessionStorage.setItem('tabCount', tabCount);
+}
+
+window.addEventListener('load', function() {
+    updateTabCount(1);
+});
+
+window.addEventListener('beforeunload', function() {
+    updateTabCount(-1);
+
+    if (parseInt(sessionStorage.getItem('tabCount')) === 0) {
         localStorage.setItem('authenticated', 'false');
-     });
+    }
+});
+
+window.addEventListener('unload', function() {
+    if (parseInt(sessionStorage.getItem('tabCount')) === 0) {
+        localStorage.setItem('authenticated', 'false');
+    }
+});
