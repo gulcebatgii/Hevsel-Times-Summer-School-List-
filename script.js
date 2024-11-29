8ca90a1fdb9d1a310ee166e7f20db173a07caadc
gulcebatgii <139014302+gulcebatgii@users.noreply.github.com>
2024-11-29 14:39:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const usernameInput = document.getElementById('username');
 const passwordInput = document.getElementById('password');
 const submitButon = document.getElementById('buton');
 submitButon.addEventListener('click', (event) => {
-    const username = usernameInput.value; 
-    const password = passwordInput.value; 
+    const username = usernameInput.value.trim();
+    const password = passwordInput.value.trim(); 
     authenticateUser (username, password);
 })
 function authenticateUser(username, password) {
